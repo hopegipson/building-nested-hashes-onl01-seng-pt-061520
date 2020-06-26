1fa2d9c09b84c7501c7d5798b5ed8ff5506eec1d
Hope Gipson <gipson.hope@gmail.com>
2020-06-26 23:41:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,13 +7,13 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {name: "Lord Capulet", age: "50"
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {name: "Lady Capulet", age: "51"
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {"Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
